384190be7f8ee79d75c263689fa0cb8b7439bc08
rangtest215 <rangtest215@gmail.com>
2025-12-10 23:30:03 +0800

    modify port

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     server: {
-        port: 5173,
+        host: '0.0.0.0',
+        port: process.env.PORT || 5173,
+    },
+    preview: {
+        host: '0.0.0.0',
+        port: process.env.PORT || 4173,
+        allowedHosts: true,
     },
 })
